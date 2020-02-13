43dd089874c451e7f41452d52db720634dde2cca
Renata Svikle <renata.svikle@inbox.lv>
2020-02-13 23:18:26 +0200

    some refactoring

--- src/main/java/lv/svikleren/roadmapapp/constants/Constants.java
+++ src/main/java/lv/svikleren/roadmapapp/constants/Constants.java
@@ -2,6 +2,10 @@ package lv.svikleren.roadmapapp.constants;
 
 public class Constants {
 
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static final String REGEX_PHONE = "^(\\+\\d{11}$)|(\\d{13})|(\\d{8})$";
     public static final String REGEX_EMAIL = "^[a-zA-Z0-9._%+-]++@[a-zA-Z0-9.-]+\\.[a-zA-Z0-9]{2,}+$";
     public static final String REDIRECT_TO_MAIN_PAGE = "redirect:/";
